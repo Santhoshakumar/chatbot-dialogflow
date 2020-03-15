3b96f7643ebbe814b714be84b9ac941b82317e63
Santhosh Kumar <35000496+Santhoshakumar@users.noreply.github.com>
2020-03-15 22:59:41 +0530

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+//token: '6bfdabf5b3b0422e8f879011806579e0'
+
 export const environment = {
   production: true,
-  token: '6bfdabf5b3b0422e8f879011806579e0'
+  token: '3c811bc0daa64edfba19c289f0b2efb4'
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The list of which env maps to which file can be found in `.angular-cli.json`.token: '6bfdabf5b3b0422e8f879011806579e0'
 
 export const environment = {
   production: false,
-  token: '6bfdabf5b3b0422e8f879011806579e0'
+  
+  token: '3c811bc0daa64edfba19c289f0b2efb4'
 };
